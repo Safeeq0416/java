fc1d82a2f9983a529fc960720e07575c4dd4b3df
safeeq0416 <psmsafeeq0416@gmail.com>
2023-01-20 10:17:42 +0000

    add java file

---
+++ charac.java
@@ -0,0 +1,38 @@
+public class charac
+{
+	public static void main(String[] args)
+	{
+		char ch1= 65;
+		String s1= "all tis main";
+		s1.toUpperCase();
+		System.out.println(s1.toUpperCase);
+		char ch2= 16;
+		char ch3= 04;
+		int i1= 'a';	
+		int i2= 'A';
+		boolean isAlive= false;
+	if(isAlive)
+	 System.out.println("alive");
+	else
+	 System.out.println("not alive");
+
+		boolean isEmpty= true;
+	if(isEmpty)
+	 System.out.println("Empty");
+	else
+	 System.out.println("not empty");
+		//boolean isEqual= true
+
+
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		System.out.println(i1);
+		System.out.println(i2);
+
+
+
+	
+
+}
+}
